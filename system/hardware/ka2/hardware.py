88a38212d8b8e5604566695d56847b9edb1f30d1
Kommu Device <device@kommu.ai>
2025-09-19 17:08:57 +0800

    LTE upload working?

--- system/hardware/ka2/hardware.py
+++ system/hardware/ka2/hardware.py
@@ -156,19 +156,14 @@ class Ka2(HardwareBase):
       elif primary_type == '802-11-wireless':
         return NetworkType.wifi
       else:
-        active_connections = self.nm.Get(NM, 'ActiveConnections', dbus_interface=DBUS_PROPS, timeout=TIMEOUT)
-        for conn in active_connections:
-          c = self.bus.get_object(NM, conn)
-          tp = c.Get(NM_CON_ACT, 'Type', dbus_interface=DBUS_PROPS, timeout=TIMEOUT)
-          if tp == 'gsm':
-            modem = self.get_modem()
-            access_t = modem.Get(MM_MODEM, 'AccessTechnologies', dbus_interface=DBUS_PROPS, timeout=TIMEOUT)
-            if access_t >= MM_MODEM_ACCESS_TECHNOLOGY_LTE:
-              return NetworkType.cell4G
-            elif access_t >= MM_MODEM_ACCESS_TECHNOLOGY_UMTS:
-              return NetworkType.cell3G
-            else:
-              return NetworkType.cell2G
+        modem = self.get_modem()
+        access_t = modem.Get(MM_MODEM, 'AccessTechnologies', dbus_interface=DBUS_PROPS, timeout=TIMEOUT)
+        if access_t >= MM_MODEM_ACCESS_TECHNOLOGY_LTE:
+          return NetworkType.cell4G
+        elif access_t >= MM_MODEM_ACCESS_TECHNOLOGY_UMTS:
+          return NetworkType.cell3G
+        else:
+          return NetworkType.cell2G
     except Exception:
       pass
 
